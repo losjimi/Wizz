3edcfd939a8ab39155cf99f99308c9f216dafd27
student <tester@tester-m>
2019-02-24 09:38:48 +0100

    Dodano szkic pythona

--- test_wizzair.py
+++ test_wizzair.py
@@ -1,1 +1,15 @@
 from selenium import webdriver
+import unittest
+
+class WizzardRegisterTest(unittest.TestCase):
+    """
+    Class for regisdtration on wizzair.com/pl-pl/
+    """
+    def setUP(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_wrong_email_(self):
+        pass
